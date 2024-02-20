ca44da6ce459fbd047db16e672a802b5936e94f1
Crowdbotics <team@crowdbotics.com>
2024-02-20 15:04:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tee-time-booking-ap-46495",
+  url: "https://tee-time-booking-ap-46495.botics.co",
+  api: "https://tee-time-booking-ap-46495.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
